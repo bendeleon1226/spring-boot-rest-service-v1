95b2f29e3b9f277bad173cfabd819dc7d2dca0e4
Ben De Leon <bendeleonjr@hotmail.com>
2021-08-24 11:30:30 +0800

    my simple restful service using springboot

---
+++ src/main/java/com/bendeleon/springbootrestservicev1/SpringBootRestServiceV1Application.java
@@ -0,0 +1,13 @@
+package com.bendeleon.springbootrestservicev1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestServiceV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestServiceV1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/bendeleon/springbootrestservicev1/SpringBootRestServiceV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bendeleon.springbootrestservicev1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestServiceV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
